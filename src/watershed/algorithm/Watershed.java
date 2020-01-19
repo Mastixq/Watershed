0747532149f81c7a15a1103e1553252a37c67898
Mastixq <mstepel95@gmail.com>
2020-01-19 22:54:58 +0100

    quick fixes vol2

--- src/watershed/algorithm/Watershed.java
+++ src/watershed/algorithm/Watershed.java
@@ -22,8 +22,11 @@ import java.util.Map;
 public class Watershed {
     private Mat processImg;
     private Pixel[][] pixelArray;
-
+    public int counterIsChecked;
+    public int counterPixels;
     public Watershed(String filename) throws IOException {
+        counterPixels = 0;
+        counterIsChecked = 0;
         Mat srcOriginal = Imgcodecs.imread(filename);
         if (srcOriginal.empty()) {
             System.err.println("Cannot read image: " + filename);
@@ -36,10 +39,11 @@ public class Watershed {
         save(pixelArray,pixelArray.length,pixelArray[0].length, "processed.png");
         System.out.println("leaving to pixel array");
         startMarkers(pixelArray);
-        System.out.println(Pixel.nextSeed);
         System.out.println("end markers");
         HashMap<Integer,Color> colorMap = Pixel.colorMap;
         save(pixelArray,pixelArray.length,pixelArray[0].length, "marked.png");
+        System.out.println(counterIsChecked);
+        System.out.println(counterPixels);
     }
     private Mat initialProcess(Mat srcOriginal) {
         processImg = srcOriginal.clone();
@@ -58,11 +62,14 @@ public class Watershed {
 
         Imgproc.erode(morph,dst,kernel);
         HighGui.imshow("After erode", dst);
-
+        Mat inverted = new Mat();
+        Core.bitwise_not(dst,inverted);
         Mat test = new Mat();
-        Imgproc.distanceTransform(dst, test, 1, 3);
+
+        Imgproc.distanceTransform(inverted, test, 1, 3);
         Mat normalized = new Mat();
         Core.normalize(test, normalized, 0, 1., Core.NORM_MINMAX);
+
         //Imgproc.sqrBoxFilter(normalized, );
         //HighGui.imshow("After distancetrnsfm", normalized);
         Imgcodecs.imwrite("resources/dstTransform.jpg",test);
@@ -75,31 +82,26 @@ public class Watershed {
     private Pixel[][] toPixelArray(Mat src){
         int width = (int)src.size().width;
         int height = (int)src.size().height;
-        System.out.println(width);
-        System.out.println(height);
-        System.out.println();
-        System.out.println();
-        System.out.println("dumping");
-        System.out.println(src.dump());
         //System.out.println("dumping over");
         Pixel[][] pixelArray = new Pixel[width][height];
         System.out.println("\n\n\n\n");
         for (int i = 0 ; i < width ; i++){
             for (int j = 0; j<height; j++){
                 //System.out.print(i+ " "+j);
-
+                counterPixels++;
                 int distance = (int)(src.get(j,i)[0]*255);
                 System.out.print(distance+" ");
-                Point tmpPoint = new Point(j,i);
-                pixelArray[i][j] = new  Pixel(Pixel.newSeed(new Color(distance,distance,distance)),              //all empty at initialize
+                Point tmpPoint = new Point(i,j);
+                pixelArray[i][j] = new  Pixel(Pixel.EMPTY,              //all empty at initialize
                                                 distance,  //
                                                 tmpPoint,
                         new Color(distance,distance,distance));
-//                if (pixelArray[i][j].distance == (double)0) {
-//                    //System.out.println("0.0");
-//                    pixelArray[i][j].isChecked = true;
-//                    pixelArray[i][j].state = Pixel.BORDER;
-//                }
+                if (pixelArray[i][j].distance == (double)0) {
+                    //System.out.println("0.0");
+                    pixelArray[i][j].isChecked = true;
+                    counterIsChecked++;
+                    pixelArray[i][j].state = Pixel.BORDER;
+                }
             }
             System.out.println("");
         }
@@ -112,22 +114,44 @@ public class Watershed {
      * @param src 2dimensional array of pixel containing preprocessed image
      */
     private void startMarkers(Pixel[][] src){ //seed it
+        int markersCount = 0;
         int width = src.length;
         int height = src[0].length;
+        counterPixels = 0;
         for (int i = 0 ; i < width ; i++){
             for (int j = 0; j<height; j++){
+                counterPixels++;
                 Pixel currPix = src[i][j];
                 //we only need to check each pixel once
-                if(currPix.isChecked) {
-                    continue;
-                }
+
                 if(checkIfMax(src,i,j)){
+                    boolean isPossible = true;
+                    System.out.println("");
+
+
                     //System.out.println("Marking for distance: "+currPix.distance);
                     //System.out.println("With position x:"+i+" y:"+j);
+                    markersCount++;
                     currPix.state=Pixel.newSeed();
+                    for(int p1=-2;p1<3;p1++) {
+                        for (int p2 = -2; p2 < 3; p2++) {
+
+
+                            int x = p1 + i;
+                            int y = p2 + j;
+                            if(x<0 || x>src.length-1 || y<0 || y>src[0].length-1) {
+                                System.out.print("NULL"+" ");
+                            }
+                            else {
+                                System.out.print(src[x][y].distance + " " + src[x][y].state + " ");
+                            }
+                        }
+                        System.out.println("");
+                    }
                 }
             }
         }
+        System.out.println("Markers: "+markersCount);
 
     }
 
@@ -142,38 +166,64 @@ public class Watershed {
     boolean checkIfMax(Pixel[][] src, int p1, int p2)
     {
         Pixel currPix = src[p1][p2];
+        if (currPix.isChecked)
+            return false;
         currPix.isChecked = true;
+        counterIsChecked++;
         int sizeX = src.length;
         int sizeY = src[0].length;
         boolean returnFlag = true;
+        int counter = 0;
+
+
         for(int i=-1;i<2;i++)
         {
             for(int j=-1;j<2;j++)
             {
+                counter++;
                 int x = p1 + i;
                 int y = p2 + j;
 
                 if(x<0 || x>sizeX-1 || y<0 || y>sizeY-1)
                     continue;
 
+                if(i==0 && j == 0){
+                    continue;
+                }
+
                 Pixel neighbouringPix = src[x][y];
+                if (neighbouringPix.state > Pixel.BORDER)
+                    return false;
+
 
                 if(neighbouringPix.distance > currPix.distance) {
 
                     returnFlag = false;
                 }
+
+                if (neighbouringPix.distance < currPix.distance && !neighbouringPix.isChecked ){
+                    neighbouringPix.isChecked = true;
+
+                    counterIsChecked++;
+                }
                 if(neighbouringPix.state==0 || neighbouringPix.state==1)
                     continue;
                 else
-                    {
-                        //just in case it's false-positive max pixel, search neighbouring pixels for any other maxima
-                        if(neighbouringPix.isChecked == false && neighbouringPix.distance == currPix.distance) {
-                            if (!checkIfMax(src, x, y))
-                                returnFlag = false;
-                        }
+                {
+                    //just in case it's false-positive max pixel, search neighbouring pixels for any other maxima
+                    if(neighbouringPix.isChecked == false && neighbouringPix.distance >= currPix.distance) {
+                        if (!checkIfMax(src, x, y))
+                            returnFlag = false;
                     }
                 }
             }
+        }
+        if (counter != 9) {
+            System.out.println("DSADBJDLSABLDHJAS");
+            System.out.println("Point:"+ currPix.pos.toString() );
+        }
+
+
         return returnFlag;
     }
 
