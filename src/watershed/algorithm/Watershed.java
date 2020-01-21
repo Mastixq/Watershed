59b4d7bdb8ace3c50d0522a85cab10415c863a87
Michal Steplewski <michal.steplewski@ericsson.com>
2020-01-21 15:00:08 +0100

    Structure changes

--- src/watershed/algorithm/Watershed.java
+++ src/watershed/algorithm/Watershed.java
@@ -1,6 +1,7 @@
 package watershed.algorithm;
 
 import watershed.operations.Pixel;
+import watershed.operations.ImageBase;
 
 import org.opencv.core.*;
 import org.opencv.highgui.HighGui;
@@ -33,94 +34,21 @@ public class Watershed {
             System.exit(0);
         }
         Mat processsed = new Mat();
-        processsed = initialProcess(srcOriginal);
+        processsed = ImageBase.initialProcess(srcOriginal);
         System.out.println("leaving initialProcess");
-        pixelArray = toPixelArray(processsed);
-        save(pixelArray,pixelArray.length,pixelArray[0].length, "processed.png");
+        pixelArray = ImageBase.toPixelArray(processsed);
+        ImageBase.save(pixelArray,pixelArray.length,pixelArray[0].length, "processed.png");
         System.out.println("leaving to pixel array");
         startMarkers(pixelArray);
         System.out.println("end markers");
         HashMap<Integer,Color> colorMap = Pixel.colorMap;
-        save(pixelArray,pixelArray.length,pixelArray[0].length, "marked.png");
+        ImageBase.save(pixelArray,pixelArray.length,pixelArray[0].length, "marked.png");
         System.out.println(counterIsChecked);
         System.out.println(counterPixels);
     }
-    private Mat initialProcess(Mat srcOriginal) {
-        processImg = srcOriginal.clone();
-        Mat gray = new Mat(srcOriginal.size(), CvType.CV_8U);
-        Mat morph = new Mat(srcOriginal.size(), CvType.CV_8U);
-        Mat laplace = new Mat();
 
 
 
-        Mat blur = new Mat();
-        Imgproc.blur(srcOriginal,blur,new Size(3,3));
-       // Imgproc.blur(blur,blur,new Size(3,3));
-       // Imgproc.blur(blur,blur,new Size(3,3));
-        HighGui.imshow("src",srcOriginal);
-        HighGui.imshow("blur",blur);
-        Imgproc.Laplacian(blur,laplace,srcOriginal.depth());
-
-        Imgproc.cvtColor(laplace,laplace,Imgproc.COLOR_BGR2GRAY);
-        Imgproc.threshold(laplace,blur,0,255,Imgproc.THRESH_OTSU);
-        HighGui.imshow("laplace", laplace);
-        Imgproc.cvtColor(srcOriginal,gray,Imgproc.COLOR_BGR2GRAY);
-        //HighGui.imshow("Color2GRAY", gray);
-        Imgproc.threshold(gray,processImg,0,255,Imgproc.THRESH_OTSU);
-        //HighGui.imshow("THRESHOLD OTSU", processImg);
-        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
-                new  Size(2, 2));
-        Imgproc.dilate(processImg,morph,kernel);
-        Mat dst = new Mat();
-        //HighGui.imshow("After dilate", morph);
-
-        Imgproc.erode(morph,dst,kernel);
-        HighGui.imshow("After erode", dst);
-        Mat inverted = new Mat();
-        Core.bitwise_not(dst,inverted);
-        Mat test = new Mat();
-
-        Imgproc.distanceTransform(inverted, test, 1, 3);
-        Mat normalized = new Mat();
-        Core.normalize(test, normalized, 0, 1., Core.NORM_MINMAX);
-
-        //Imgproc.sqrBoxFilter(normalized, );
-        //HighGui.imshow("After distancetrnsfm", normalized);
-        Imgcodecs.imwrite("resources/dstTransform.jpg",test);
-        HighGui.waitKey( 0 );
-        System.out.println("Normalized: ");
-        System.out.println(normalized.dump());
-        return normalized;
-    }
-
-    private Pixel[][] toPixelArray(Mat src){
-        int width = (int)src.size().width;
-        int height = (int)src.size().height;
-        //System.out.println("dumping over");
-        Pixel[][] pixelArray = new Pixel[width][height];
-        System.out.println("\n\n\n\n");
-        for (int i = 0 ; i < width ; i++){
-            for (int j = 0; j<height; j++){
-                //System.out.print(i+ " "+j);
-                counterPixels++;
-                int distance = (int)(src.get(j,i)[0]*255);
-                System.out.print(distance+" ");
-                Point tmpPoint = new Point(i,j);
-                pixelArray[i][j] = new  Pixel(Pixel.EMPTY,              //all empty at initialize
-                                                distance,  //
-                                                tmpPoint,
-                        new Color(distance,distance,distance));
-                if (pixelArray[i][j].distance == (double)0) {
-                    //System.out.println("0.0");
-                    pixelArray[i][j].isChecked = true;
-                    counterIsChecked++;
-                    pixelArray[i][j].state = Pixel.BORDER;
-                }
-            }
-            System.out.println("");
-        }
-        return pixelArray;
-    }
 
     /**
      * Function responsible for creating one point markers
@@ -233,10 +161,6 @@ public class Watershed {
                 }
             }
         }
-        if (counter != 9) {
-            System.out.println("DSADBJDLSABLDHJAS");
-            System.out.println("Point:"+ currPix.pos.toString() );
-        }
 
 
         return returnFlag;
@@ -246,30 +170,8 @@ public class Watershed {
 
     }
 
-    public static Mat BufferedImage2Mat(BufferedImage image) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ImageIO.write(image, "jpg", byteArrayOutputStream);
-        byteArrayOutputStream.flush();
-        return Imgcodecs.imdecode(new MatOfByte(byteArrayOutputStream.toByteArray()),  -1);
-    }
-    public static BufferedImage Mat2BufferedImage(Mat matrix)throws IOException {
-        MatOfByte mob=new MatOfByte();
-        Imgcodecs.imencode(".jpg", matrix, mob);
-        return ImageIO.read(new ByteArrayInputStream(mob.toArray()));
-    }
 
-    BufferedImage save(Pixel[][] src, int width, int height, String filename) throws IOException{
-        BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                int rgb=Pixel.colorMap.get((pixelArray[i][j].state)).getRGB();
-                newImage.setRGB(i, j, rgb);
-            }
-        }
-        File outfile = new File(filename);
-        ImageIO.write(newImage, "png", outfile);
-        return newImage;
-    }
+
 
 
 }
